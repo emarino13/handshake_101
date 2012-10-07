de41aa5f565bf7b365c4dd048e9d735623f85321
Elaine Marino <elaine.c.marino@gmail.com>
2012-10-06 18:07:33 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Handshake101::Application.config.secret_token = '5991c60a3a3d794f56a64f9745a5d9a87c1c80ddf0111b42bd8803018599b38fb8ba9a7ebd62f83a0b9495339493f5e213e61fbbc38442d7be1e2c932476ac3b'
